54221467e727ff58be9dcee8aaed62249597116e
j1python <jaime.pedroza1@gmail.com>
2020-08-12 15:44:08 -0500

    Portfo vr 1

---
+++ server.py
@@ -0,0 +1,40 @@
+import csv
+
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data["email"]
+#         subjects = data["subjects"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{subjects},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subjects = data["subjects"]
+        message = data["message"]
+        cvs_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #cvs_writer.writerow([])
+        cvs_writer.writerow([email,subjects,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again!'
